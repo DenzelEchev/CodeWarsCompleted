5994059b3de593fdf56e2e4ba3bed549ae256082
Denzel M. Echevarria <77076209+DenzelEchev@users.noreply.github.com>
2025-07-21 01:04:11 -0400

    Update answer4.js
    one liner on the answer

--- answers/answer4.js
+++ answers/answer4.js
@@ -10,3 +10,6 @@ function sentencify(words) {
   let sentence = words.join(' ')
   return sentence.charAt(0).toUpperCase() + sentence.slice(1) + '.'
 }
+
+//Refractor 2
+const sentencify = (words) => words.join(' ').charAt(0).toUpperCase() + words.join(' ').slice(1) + '.'
